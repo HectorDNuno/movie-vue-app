d8ccd55a19acce4b87517fd2ef83ad2fac41263a
hector <hectornuno1994@gmail.com>
2022-04-11 16:56:02 -0700

    adds ability to see all movies

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,18 @@
 <script>
+import axios from "axios";
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
+      message: "Lets see some movies!",
+      movies: [],
     };
   },
-  created: function () {},
+  created: function () {
+    axios.get("/movies.json").then((response) => {
+      this.movies = response.data;
+      console.log("All movies", this.movies);
+    });
+  },
   methods: {},
 };
 </script>
@@ -13,6 +20,12 @@ export default {
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="movie in movies" :key="movie.id">
+      <p>Title: {{ movie.title }}</p>
+      <p>Year: {{ movie.year }}</p>
+      <p>Plot: {{ movie.plot }}</p>
+      <p>Director: {{ movie.director }}</p>
+    </div>
   </div>
 </template>
 
