4de5ed2dffb7bf387cd865ab409fd48286428727
hector <hectornuno1994@gmail.com>
2022-04-11 17:28:14 -0700

    adds create actions

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,6 +5,7 @@ export default {
     return {
       message: "Lets see some movies!",
       movies: [],
+      newMovieParams: {},
     };
   },
   created: function () {
@@ -13,13 +14,39 @@ export default {
       console.log("All movies", this.movies);
     });
   },
-  methods: {},
+  methods: {
+    addMovie: function () {
+      axios
+        .post("/movie.json", this.newMovieParams)
+        .then((response) => {
+          console.log("Movie added!", response.data);
+          this.movie.push(response.data);
+        })
+        .catch((error) => console.log(error.response));
+    },
+  },
 };
 </script>
 
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <button v-on:click="addMovie">Add a movie!</button>
+
+    <div>
+      Title:
+      <input type="text" v-model="newMovieParams.title" />
+      <br />
+      Year:
+      <input type="text" v-model="newMovieParams.year" />
+      <br />
+      Plot:
+      <input type="text" v-model="newMovieParams.Plot" />
+      <br />
+      Director:
+      <input type="text" v-model="newMovieParams.director" />
+    </div>
+
     <div v-for="movie in movies" :key="movie.id">
       <p>Title: {{ movie.title }}</p>
       <p>Year: {{ movie.year }}</p>
