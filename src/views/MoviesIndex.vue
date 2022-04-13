23a66255f9a20def753f43b200ab6c3a9a66796e
hector <hectornuno1994@gmail.com>
2022-04-13 15:32:01 -0700

    adds movieindex page

---
+++ src/views/MoviesIndex.vue
@@ -0,0 +1,28 @@
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      message: "Lets see some movies!",
+      movies: [],
+    };
+  },
+  created: function () {
+    axios.get("/movies.json").then((response) => {
+      console.log("all movies", response);
+      this.movies = response.data;
+    });
+  },
+};
+</script>
+
+<template>
+  <div class="movies-index">
+    <div v-for="movie in movies" :key="movie.id">
+      <h4>{{ movie.title }}</h4>
+      <p>{{ movie.year }}</p>
+      <p>{{ movie.plot }}</p>
+      <p>{{ movie.director }}</p>
+    </div>
+  </div>
+</template>
